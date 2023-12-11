869bbb3ce1efd662e389cce9e546d1fddb1e8dc5
emptyhead <korea6370@yu.ac.kr>
2023-12-11 21:44:33 +0900

    23/12/11 update

--- components/api/proxy.js
+++
@@ -1,9 +0,0 @@
-const fetch = require('node-fetch');
-
-module.exports = async (req, res) => {
-  const response = await fetch('https://craftzdog.global.ssl.fastly.net/homepage/dog.glb');
-  const data = await response.buffer();
-
-  res.setHeader('Content-Type', 'model/gltf-binary');
-  res.send(data);
-};
